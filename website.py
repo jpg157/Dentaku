8427c2547a930b1c20d463a795742b02deb5b243
Frank Hui <huifrank@live.com>
2020-01-18 14:40:22 -0800

    Add line break

--- website.py
+++ website.py
@@ -6,7 +6,7 @@ from fbchat import Mention
 class website(Command):
 
     def run(self):
-        response_text = "@" + self.author.first_name + "Check our website out at https://vikingsdev.ca\n Events: " \
+        response_text = "@" + self.author.first_name + "\nCheck our website out at https://vikingsdev.ca\n Events: " \
                                                        "vikingsdev.ca/events\n Workshops: vikingsdev.ca/workshops\n " \
                                                        "Sign Up: vikingsdev.ca/signup "
         mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]

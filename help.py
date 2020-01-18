e0512716a21d54098876bfa42ccee4f17a46f151
Frank Hui <huifrank@live.com>
2020-01-18 13:26:59 -0800

    Move to new line seperated from @mention

--- help.py
+++ help.py
@@ -6,7 +6,7 @@ from fbchat import Mention
 class help(Command):
 
     def run(self):
-        response_text = "@" + self.author.first_name + " !website: Website Info\n!help: Shows list of commands."
+        response_text = "@" + self.author.first_name + "\n!website: Website Info\n!help: Shows list of commands."
         mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
 
         self.client.send(

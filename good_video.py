d852cb39ded5f7d0e4b8cd3083a82c5386bdd490
Jason Lau <58371585+jpg157@users.noreply.github.com>
2020-01-19 01:17:46 -0800

    Update good_video.py

--- good_video.py
+++ good_video.py
@@ -6,7 +6,7 @@ from fbchat import Mention
 class good_video(Command):
 
     def run(self):
-        response_text = "@" + self.author.first_name + "Good Video Here:\nhttps://www.youtube.com/watch?v=3CQyYE1rMJg&t=6s"
+        response_text = "@" + self.author.first_name + "\nGood Video Here:\nhttps://www.youtube.com/watch?v=3CQyYE1rMJg&t=6s"
         mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
 
         self.client.send(

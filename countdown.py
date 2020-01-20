523443e0060cb90270d39742824832fdc89e5d1c
CaiCharley <32006851+CaiCharley@users.noreply.github.com>
2020-01-19 21:29:08 -0800

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -5,17 +5,19 @@ from fbchat import Mention
 class countdown(Command):
 
     def run(self):
-        count = int(self.user_params[0])
-        if count > 20:
-            response_text = "I'm too lazy to do that..." 
-        elif count <= 0:
-            response_text = "Lets do it again!"
-        else:
-            response_text = "!countdown " + str(count - 1)
-        mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
-
+        try
+            count = int(self.user_params[0])
+            if count > 10:
+                response_text = "I'm too lazy to do that..." 
+            elif count <= 0:
+                response_text = "Lets do it again!"
+            else:
+                response_text = "!countdown " + str(count - 1)
+            mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
+        except
+            response_text = "You think you're soooo clever? Not anymore " + self.author.first_name ", because I now have error catching!"
         self.client.send(
-            Message(text=response_text, mentions=mentions),
+            Message(text=response_text),
             thread_id=self.thread_id,
             thread_type=self.thread_type
         )
